d883ae733634c853104b45ae65c0c7d33ed21479
Dkazem91 <Dkazemian@gmail.com>
2018-03-16 18:26:54 +0000

    realloc is now helperfunction _realloc, legal

--- simpleShell.c
+++ simpleShell.c
@@ -4,6 +4,30 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <string.h>
+void *_realloc(void *ptr, unsigned int new_size)
+{
+	void *ret = 0;
+	char *write, *read;
+
+	if (new_size > 0 || ptr == 0)
+	{
+		ret = malloc(new_size);
+		if (ret == 0)
+			return (0);
+	}
+	if (new_size > 0 && ptr != 0)
+	{
+		write = ret;
+		read = ptr;
+		while (new_size)
+		{
+			new_size--;
+			*write++ = *read++;
+		}
+	}
+	free(ptr);
+	return (ret);
+}
 int main (void)
 {
 	int i = 0;
@@ -23,17 +47,19 @@ int main (void)
 			while (token != NULL)
 			{
 				argv[i] = token;
-				argv = realloc(argv,++n * sizeof(char *));
+				argv = _realloc(argv,++n * sizeof(char *));
 				token = strtok(NULL, " \n");
 				i++;
 			}
 			argv[i] = NULL;
 			execve(argv[0],argv,NULL);
-			free(argv);
 		}
 		else
 		{
 			wait(NULL);
 		}
 	}
+
+	free(argv);
+	return(0);
 }
